b74a713f11e30d29f5d638e766a9274fc84b1e14
Aditya Mathpal <adimathpal@gmail.com>
2024-08-25 15:22:25 +0530

    Fixed errors in Q2.

--- Lab 4/Q2/Q2.v
+++ Lab 4/Q2/Q2.v
@@ -1,33 +1,32 @@
-`include "dec2to4.v"
+`include "Q1.v"
 module Q2(
     input [3:0] W,
     input En,
     output [15:0] Y,
     output [3:0] x
     );
-	wire [3:0] x;
 	integer k;
-	dec2to4 func(
+	Q1 func(
 	    .W(W[3:2]),
 	    .Y(x),
 	    .En(En)
 	);
-	dec2to4 d3(
+	Q1 d3(
         .W(W[1:0]),
         .Y(Y[15:12]),
         .En(x[3])
     );
-	dec2to4 d2(
+	Q1 d2(
         .W(W[1:0]),
         .Y(Y[11:8]),
         .En(x[2])
     );
-    dec2to4 d1(
+    Q1 d1(
         .W(W[1:0]),
         .Y(Y[7:4]),
         .En(x[1])
     );
-    dec2to4 d0(
+    Q1 d0(
         .W(W[1:0]),
         .Y(Y[3:0]),
         .En(x[0])
