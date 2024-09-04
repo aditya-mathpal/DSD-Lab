f6a9f47101471b5ca73166d277cb6a17e5ca64d6
Aditya Pankaj Mathpal <148540279+aditya-mathpal@users.noreply.github.com>
2024-09-04 10:55:14 +0530

    Add files via upload

--- Lab 6/Q3/Q3.v
+++ Lab 6/Q3/Q3.v
@@ -2,6 +2,7 @@ module Q3(J,K,Clk,Reset,Q);
     input J,K,Clk,Reset;
     output reg Q;
     always @(posedge Clk or posedge Reset)
-        if(J^K) Q = J;
-        else if(J) Q = ~Q;
+        if(Reset)
+            if(J^K) Q = J;
+            else if(J) Q = ~Q;
 endmodule
